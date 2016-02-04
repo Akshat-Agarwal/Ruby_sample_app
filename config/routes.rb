0ce572811f6629aaab44141afba7f8711a808848
Akshat Agarwal <akshat_agarwal@hotmail.com>
2016-02-04 01:04:05 +0000

    Added test cases for about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get 'static_pages/about'
+  
+  root 'application#hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -57,6 +61,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
-  root 'application#hello'
 end
